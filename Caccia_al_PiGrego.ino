c5825aa422b0f7e14fdc2564ed8549765714d169
Edampadam Manu SIbi <manusibyedampadam@yahoo.it>
2019-04-10 18:21:37 +0200

    Starting

---
+++ Caccia_al_PiGrego.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+
+int btnpos1 = 1;
+int btnpos2 = 2;
+int btnpos3 = 3;
+int btnpos4 = 4;
+int bntpos5 = 5;
+int vite = 0;
+int jolly = 0;
+int penality = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void 
