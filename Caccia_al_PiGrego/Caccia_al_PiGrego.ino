a3eea25a63b0a46d8ec1c2d932a358716ff9905e
Edampadam Manu SIbi <manusibyedampadam@yahoo.it>
2019-05-24 19:25:37 +0200

    Edampadam sesto

--- Caccia_al_PiGrego/Caccia_al_PiGrego.ino
+++ Caccia_al_PiGrego/Caccia_al_PiGrego.ino
@@ -1,5 +1,5 @@
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(8,9,10,11,12,13);
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 int btnpos1 = 1;
 int btnpos2 = 2;
@@ -7,17 +7,15 @@ int btnpos3 = 3;
 int btnpos4 = 4;
 int btnpos5 = 5;
 int btnstart = 6;
-char A = '$';
-char B = '+';
-char C = '-';
-int tempo = 1000;
+int tempo = 4000;
 int vite = 5;
 int punteggio = 0;
 int jolly = 0;
 int penality = 0;
 int record = 0;
 int posizioneRandom = 0;
-int contatempo = 0;
+int tempopassato = 0;
+int a = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,7 +24,15 @@ void setup() {
   pinMode (btnpos3, INPUT);
   pinMode (btnpos4, INPUT);
   pinMode (btnpos5, INPUT);
-  lcd.begin (16, 2);
+  pinMode (btnstart, INPUT);
+  vite = 5;
+  punteggio = 0;
+  jolly = 0;
+  penality = 0;
+  record = 0;
+  posizioneRandom = 0;
+  lcd.init();
+  lcd.backlight();
 
 }
 
@@ -34,145 +40,127 @@ void loop() {
   // put your main code here, to run repeatedly:
   vite = 5;
   punteggio = 0;
-  lcd.setCursor (0,0);
-  lcd.print("Vite: 5");
-  lcd.setCursor (6,0);
-  lcd.print ("Record:" + record);
-  if (digitalRead (btnstart) == HIGH)
-   {
-   while(vite>0);
-   {
-    Random(1000);
-    CaratterePigreco(1000,5);
-    CarattereBonus();
-    CaratterePenality();
-   }
-
-   if (punteggio >record)
-   {
-    record = punteggio;
-   }
-    lcd.setCursor(0,0);
-    lcd.print("Vite:"+ vite);
-    lcd.setCursor(6,0);
-    lcd.print("Record:"+ record);
+  lcd.setCursor (0, 0);
+  lcd.print("Vite:" + String(vite));
+  lcd.setCursor (8, 0);
+  lcd.print ("Record:" + String(record));
+  if (digitalRead(btnstart) == HIGH)
+  {
+    while (vite > 0);
+    {
+      Random();
+      lcd.setCursor(0, 0);
+      lcd.print("Vite:" + vite);
+      lcd.setCursor(6, 0);
+      lcd.print("Record:" + record);
+      if (punteggio > record)
+      {
+        record = punteggio;
+      }
+    }
   }
 }
 
-void Random (int posizioneRandom) {
-int posizoneRandom = random (1, 6);
-lcd.setCursor (1, posizoneRandom);
-lcd.print ("$"|| "+" || "-");
- 
+void Random() {
+  posizioneRandom = random (1, 6);
+  lcd.setCursor (1, (posizioneRandom * 3) - 1);
+  if (posizioneRandom == 1)
+  {
+    lcd.print("+");
+    CarattereBonus(posizioneRandom);
+  }
+  else if (posizioneRandom == 2)
+  {
+    lcd.print("-");
+    CaratterePenality();
+  }
+  else
+  {
+    lcd.print("$");
+    CaratterePigreco(posizioneRandom);
+  }
 }
 
-void Tempo() {     
-if (digitalRead('btnpos1' || 'botnpos2' || 'btnpos3' || 'btnpos4' || 'btnpos5') == HIGH)
-{
-  tempo --;
-  delay (1);
-}
-}
-void CaratterePigreco(int posizioneRandom, int punteggio) {                                                        
-if ( posizioneRandom == 1 && posizioneRandom == A)
-         {
-          if (digitalRead(btnpos1) == HIGH)
-           {
-             punteggio++;
-           } 
-          else if (tempo < contatempo)
-           {
-             vite--;
-           }
-         }
-if ( posizioneRandom == 2 && posizioneRandom == A)
-        {
-          if (digitalRead(btnpos2) == HIGH)
-           {
-             punteggio++;
-           }
-          else if (tempo < contatempo)
-           {
-             vite--;
-           }
-        }
-if ( posizioneRandom == 3 && posizioneRandom == A)
-        {
-          if (digitalRead(btnpos3) == HIGH)
-           {
-             punteggio++;
-           }
-          else if (tempo < contatempo)
-           {
-             vite--;
-           }
-        }
-if ( posizioneRandom == 4 && posizioneRandom == A)
-        {
-          if (digitalRead(btnpos4) == HIGH)
-           {
-             punteggio++;
-           }
-          else if (tempo < contatempo)
-           {
-             vite--;
-           }
-        }
-if ( posizioneRandom == 5 )
-        {
-          if (digitalRead(btnpos5) == HIGH)
-           {
-             punteggio++;
-           }
-          else if (tempo < contatempo)
-           {
-             vite--;
-           }
-        }
+void CaratterePigreco (int pos) {
+  while (tempopassato < tempo)
+  {
+    if (digitalRead(pos) == HIGH)
+    {
+      punteggio++;
+      tempo = tempo - 30;
+      break;
     }
-        
-
-void CarattereBonus() {
- if  ( posizioneRandom == 1 && posizioneRandom == B && digitalRead(btnpos1) == HIGH)
- {
- vite++;
- }   
- if  ( posizioneRandom == 2 && posizioneRandom == B && digitalRead(btnpos2) == HIGH)
- {
- vite++;
- }             
- if  ( posizioneRandom == 3 && posizioneRandom == B && digitalRead(btnpos3) == HIGH)
- {
- vite++;
- }      
- if  ( posizioneRandom == 4 && posizioneRandom == B && digitalRead(btnpos4) == HIGH)
- {
- vite++;
- }      
- if  ( posizioneRandom == 5 && posizioneRandom == B && digitalRead(btnpos5) == HIGH)
- {
- vite++;
- }      
+    else
+    {
+      for (a = 1; a < 6; a++)
+      {
+        if (digitalRead(a) == HIGH && pos != a)
+        {
+          vite--;
+          tempo = tempo - 30;
+          break;
+        }
+      }
+      if (a != 6)
+      {
+        break;
+      }
+      tempopassato++;
+      delay(1);
+    }
+    if (tempopassato == tempo)
+    {
+      vite--;
+      tempo = tempo - 30;
+    }
+  }
 }
 
-void CaratterePenality() {
-  if  ( posizioneRandom == 1 && posizioneRandom == C && digitalRead(btnpos1) == HIGH)
- {
- vite--;
- }   
- if  ( posizioneRandom == 2 && posizioneRandom == C && digitalRead(btnpos2) == HIGH)
- {
- vite--;
- }             
- if  ( posizioneRandom == 3 && posizioneRandom == C && digitalRead(btnpos3) == HIGH)
- {
- vite--;
- }      
- if  ( posizioneRandom == 4 && posizioneRandom == C && digitalRead(btnpos4) == HIGH)
- {
- vite--;
- }      if  ( posizioneRandom == 5 && posizioneRandom == C && digitalRead(btnpos5) == HIGH)
- {
- vite--;
- }      
+void CarattereBonus(int pos) {
+  while (tempopassato < tempo)
+  {
+    if (digitalRead(pos) == HIGH)
+    {
+      vite++;
+      break;
+    }
+    else
+    {
+      for (a = 1; a < 6; a++)
+      {
+        if (digitalRead(a) == HIGH && pos != a)
+        {
+          vite--;
+          tempo = tempo - 30;
+          break;
+        }
+      }
+      if (a != 6)
+      {
+        break;
+      }
+      tempopassato++;
+      delay(1);
+    }
+  }
+}
+void CaratterePenality () {
+  while (tempopassato < tempo)
+  {
+    for (a = 1; a < 6; a++)
+    {
+      if (digitalRead(a) == HIGH)
+      {
+        vite--;
+        tempo = tempo - 30;
+        break;
+      }
+    }
+    if (a != 6)
+    {
+      break;
+    }
+    tempopassato++;
+    delay(1);
+  }
 }
